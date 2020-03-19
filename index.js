1c76e1dda6e1cf035ef70a05f72eb9ea6e77d2f4
agar <37141685+idencosmos@users.noreply.github.com>
2020-03-19 10:22:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 var steem = require('steem');
-var cnt=0;
-var query = {
-  "tag": "",
-  "limit": 100
-};
 
-function updateSteemArticles(username) {  
-  query.tag= username;
-  steem.api.getDiscussionsByBlog(query, function(err, result) {
-    for (var i = 0; i < result.length-1; i++) {
+function updateSteemArticles(username) {
+  steem.api.getDiscussionsByBlog({limit:100, tag:username}, function(err, result) {
+    for (var i = 0; i < result.length; i++) {
       const { title, body, category, author, permlink, created, json_metadata } = result[i];
       if (result[i].author == username || hexo.config.steem_resteems) {
         const tags = JSON.parse(json_metadata).tags || [];
@@ -26,34 +20,6 @@ function updateSteemArticles(username) {
           author,
         }, true)
       }
-    }
-    cnt=result.length;
-    if(cnt<100){
-      const { title, body, category, author, permlink, created, json_metadata } = result[cnt-1];
-      if (result[cnt-1].author == username || hexo.config.steem_resteems) {
-        const tags = JSON.parse(json_metadata).tags || [];
-        const date = new Date(`${created}Z`);
-        const content = body.replace(/\|/g, '|').replace(/%/g, '％').replace(/{/g, '｛').replace(/}/g, '｝');
-        // let t = title.replace(/"(.*)"/g, '“$1”').replace(/"/g, '“');//.replace(/\[|\]|:|-|#|\(|\)|\'/g, '').replace('?', '').replace('?', '');
-        // console.log(t, tags);
-        hexo.post.create({
-          path: '_steemit/' + `${created.replace('T', '-')}-${category}`,
-          title: title.replace(/"(.*)"/g, '“$1”').replace(/"/g, '“'),
-          content,
-          date,
-          category: 'Uncategorized',
-          tags,
-          author,
-        }, true)
-      }   
-      return;
-    }
-    else{
-      query.start_author= result[cnt-1].author; 
-      query.start_permlink= result[cnt-1].permlink;
-      console.log(query);
-
-      updateSteemArticles(username); 
     }
   });
 }
